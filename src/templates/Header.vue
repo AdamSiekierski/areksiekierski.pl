971664872c9e5245911da3b594c72faf38cdad2e
AdamSiekierski <adam.siekiera@outlook.com>
2019-05-22 19:59:33 +0200

    Primitive project setup

---
+++ src/templates/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="headerWrapper">
+    <h2>I'm header</h2>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Header',
+};
+</script>
+<style lang="scss" scoped>
+#headerWrapper {
+  position: fixed;
+  height: 100px;
+}
+</style>
---
+++ src/templates/MainTemplate.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="main">
+    <Header />
+    <slot></slot>
+  </div>
+</template>
+<script>
+import Header from './Header.vue';
+
+export default {
+  name: 'main-template',
+  components: {
+    Header: Header,
+  },
+};
+</script>
+<style lang="scss">
+@import "../assets/styles/GlobalStyles.scss";
+</style>
