8ee561d5982ada0417cf06abeaac3425eb9e65be
Adam Siekierski <a@siekierski.ml>
2020-11-28 16:16:00 +0100

    Hamburger animation duration

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -120,7 +120,7 @@ export default {
     align-items: center;
     right: 20px;
     position: absolute;
-    transition: height 0.3s ease-in-out;
+    transition: height 0.2s ease-in-out;
   }
 
   @media #{$mq-mobile} {
